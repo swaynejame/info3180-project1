5b0726c261a3d54dc8e145fed01694bff694e3f5
swaynejame <swaynejame11@gmail.com>
2020-03-11 16:27:43 -0500

    trial 1

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField, SelectField,FileField, FileRequired,FileAllowed
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
 
 class ProfileForm(FlaskForm):

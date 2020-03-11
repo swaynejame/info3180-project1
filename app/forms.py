76e06c3bb44707125c77fbb39d18bb39afeeae00
swaynejame <swaynejame11@gmail.com>
2020-03-11 12:14:20 -0500

    trial 1

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
+from wtforms import StringField, TextField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
 
 class ProfileForm(FlaskForm):
-    name = StringField('Name',validators = [DataRequired()])
+    firstname = StringField('First Name',validators = [DataRequired()])
+    lastname = StringField('Last Name',validators = [DataRequired()])
+    gender = SelectField('Title', [DataRequired()],choices=[('Male', 'male'),('Female', 'female')])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    subject = StringField('Subject',validators = [DataRequired()])
-    message = TextField('Message',validators = [DataRequired()])
-    send = SubmitField('Send')
+    location = StringField('Location',validators = [DataRequired()])
+    biography = TextField('Biography',validators = [DataRequired()])

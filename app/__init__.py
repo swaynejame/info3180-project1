eee6e86258808fca0893af8253147bea06576eeb
swaynejame <swaynejame11@gmail.com>
2020-04-13 13:03:42 -0500

    Trial 2

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'some$3cretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fvshwbumwxfkyf:c1a66a955b43af8f4eb347230569d122e042c74a93db022d3e82da2e8489d672@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d15u2sbqj36mo3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tfelzzuhxvbbcs:a9f0bc5e6515647d3162d425c5aee205af02f01a2aa3f6efb7ea796a6c15bf89@ec2-52-202-146-43.compute-1.amazonaws.com:5432/dfar6q5fcsnu4a'
 
 # 'postgresql://admin:password@localhost/project1'
 db = SQLAlchemy(app)

18c8939a2806f0c38792ba08fc0e660317a3c35a
swaynejame <swaynejame11@gmail.com>
2020-04-13 12:55:15 -0500

    Trial 2

--- app/views.py
+++ app/views.py
@@ -25,7 +25,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Swayne James")
 
 @app.route('/profile',methods=['GET', 'POST'])
 def profile():
